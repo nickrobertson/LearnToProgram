45aed930733ebc492d8046de1b5d477523829510
nickrobertson <nbr1983@gmail.com>
2013-07-16 07:38:36 -0600

    Chapter 3

---
+++ chap3.rb
@@ -0,0 +1,34 @@
+# Simple strings
+puts 'Hello, world!'
+puts ''
+puts 'Good-bye'
+
+# Add strings
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.'
+puts 'I like' + 'apple pie.'
+
+# Mulp. strings
+puts 'blink ' * 4
+
+# 12 vs '12'
+puts 12 + 12
+puts '12' + '12'
+puts '12 + 12'
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+# Problems
+#puts '12' + 12
+#puts '2' * '5'
+#puts 'Betty' + 12
+#puts 'Fred' * 'John'
+
+#Escape
+#puts 'You're swell!'
+puts 'You\'re swell!'
+puts 'backslash at the end of a string //'
+puts 'up\\down'
+puts 'up\down'
