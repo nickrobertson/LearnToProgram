b3c96b486b2a4c998fde0066d0ed1f8ca1f057b5
nickrobertson <nbr1983@gmail.com>
2013-07-15 22:41:11 -0600

    Chapter Two Excercises

---
+++ calc.rb
@@ -0,0 +1,38 @@
+#integers
+puts "intergers"
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+#floats
+puts "floats"
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+#complex expressions
+puts "complex expressions"
+puts 5 * (12-8) + -15
+puts 98 + (59872 / (13*8)) * -51
+
+
+#hours in a year
+#24 hours in a day * 365 days in a year
+puts "Hours in a year"
+puts 24 * 365
+
+#Minutes in a decade
+#60 min 1 hour * 24 hours in a day * 365 days in a year * 10 years in a decade
+puts "Minutes in a decade"
+puts 60 * 24 * 365 * 10
+
+#My age in seconds
+#60 seconds in a minute * 60 minutes in a hour * 24 hours in a day 
+# * 365 in a year * 30 years
+puts "My age in seconds"
+puts 60 * 60 * 24 *365 * 30
+
+puts "Age of author"
+puts ((((1160000000 / 365) / 24) / 60 ) / 60)
