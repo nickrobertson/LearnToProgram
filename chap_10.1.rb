7b78d6058d42145939ed6ee9bb5797c91c783974
nick.robertson <nick.robertson@octanner.com>
2014-01-15 13:06:25 -0700

    Renamed files for Learn to Program book. Added files for the chap 10 exercises.

---
+++ chap_10.1.rb
@@ -0,0 +1,1 @@
+#Shuffle. Now that you’ve finished your new sorting algorithm, how about the opposite? Write a shuffle method that takes an array and returns a to- tally shuffled version. As always, you’ll want to test it, but testing this one is trickier: How can you test to make sure you are getting a perfect shuffle? What would you even say a perfect shuffle would be? Now test for it.
